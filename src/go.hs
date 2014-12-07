9ba9760eb707b1c79fadbe3f20cf46c111380547
Helge Rausch <helge@rausch.io>
2014-12-07 11:45:26 +0100

    Store moves and current game state in Game

--- src/go.hs
+++ src/go.hs
@@ -1,10 +1,28 @@
-data X = A | B | C | D | E | F | G | H | J | K | L | M | N | O | P | Q | R | S | T deriving Show
-data Y = I1 | I2 | I3 | I4 | I5 | I6 | I7 | I8 | I9 | I10 | I11 | I12 | I13 | I14 | I15 | I16 | I17 | I18 | I19 deriving Show
-data Move = Move X Y deriving Show
-data Game = Game [Move] deriving Show
+module Go (new_game, add_move) where
+
+import qualified Data.Map.Strict as Map
+
+data X = A | B | C | D | E | F | G | H | J | K | L | M | N | O | P | Q | R | S | T deriving (Show, Ord, Eq)
+type Y = Int
+type Coordinate = (X, Y)
+data Stone = Black | White deriving Show
+data Game = Game {
+  board :: Map.Map Coordinate Stone,
+  moves :: [Coordinate]
+} deriving Show
 
 new_game :: Game
-new_game = Game []
+new_game = Game {
+  board = Map.empty,
+  moves = []
+}
 
-move :: Game -> Move -> Game
-move (Game moves) m = Game (m:moves)
+add_move :: Game -> Coordinate -> Game
+add_move (Game { moves = moves, board = board }) move =
+  Game { moves = move:moves, board = (Map.insert move (next_stone moves) board) }
+
+next_stone :: [Coordinate] -> Stone
+next_stone moves
+  | even (length moves) = Black
+  | otherwise = White
+
