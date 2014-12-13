14e3771e4790e28e5331336645c18f226ebc599a
Helge Rausch <helge@rausch.io>
2014-12-13 13:19:40 +0100

    Draw the board using Show

--- src/go.hs
+++ src/go.hs
@@ -1,14 +1,24 @@
 module Go (new_game, add_move) where
 
 import qualified Data.Map.Strict as Map
+import Data.List
 
 data Point = Point (Int, Int) deriving (Show, Ord, Eq)
-data Stone = Black | White deriving Show
+data Stone = Black | White | Ko deriving Eq
 data Game = Game {
   board :: Map.Map Point Stone,
   moves :: [Point],
   size :: Int
-} deriving Show
+}
+
+instance Show Stone where
+  show Black = "○"
+  show White = "●"
+  show Ko = "□"
+
+instance Show Game where
+  show game@Game { size = size } =
+    intercalate "\n" [ concat [draw_board game (Point (x, y)) | x <- [1..size] ] | y <- [1..size] ]
 
 new_game :: Game
 new_game = Game {
@@ -30,4 +40,24 @@ next_stone :: [Point] -> Stone
 next_stone moves
   | even (length moves) = Black
   | otherwise = White
+
+draw_board :: Game -> Point -> String
+draw_board (Game { board = board, size = size }) point
+  | (Map.lookup point board) == Nothing = [board_at size point]
+  | otherwise = show (definitely (Map.lookup point board))
+
+board_at :: Int -> Point -> Char
+board_at size (Point (x, y))
+  | x == 1 && y == 1 = '┌'
+  | x == 1 && y == size = '└'
+  | x == size && y == 1 = '┐'
+  | x == size && y == size = '┘'
+  | y == 1 = '┬'
+  | y == size = '┴'
+  | x == 1 = '├'
+  | x == size = '┤'
+  | otherwise = '┼'
+
+definitely :: Maybe a -> a
+definitely (Just a) = a
 
