d0958d6155e0bd622a8b5d66369f1d2452af3d1d
Helge Rausch <helge@rausch.io>
2015-03-04 18:55:54 +0100

    Add test for removing of a dead stone

--- spec/Spec.hs
+++ spec/Spec.hs
@@ -21,6 +21,18 @@ main = hspec $ do
             let point = Point (20, 4) in
                 (addMove newGame point) `shouldBe` (Left "Invalid coordinates")
 
+        it "removes a single dead stone" $ do
+            let game0 = newGame
+                Right game1 = addMove game0 $ Point (4, 4)
+                Right game2 = addMove game1 $ Point (4, 3)
+                Right game3 = pass game2
+                Right game4 = addMove game3 $ Point (4, 5)
+                Right game5 = pass game4
+                Right game6 = addMove game5 $ Point (3, 4)
+                Right game7 = pass game6
+                Right game8 = addMove game7 $ Point (5, 4) in
+                boardAt game8 (Point (4, 4)) `shouldBe` Nothing
+
     describe "Go.pass" $ do
         it "lets the player pass" $ do
             let Right (Game { moves = moves }) = pass newGame in
