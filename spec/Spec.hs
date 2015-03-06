f298c7f71f3db5eaef52809a3921ab20b8d2096a
Helge Rausch <helge@rausch.io>
2015-03-06 08:12:58 +0100

    Add `addMoves` function for testing

--- spec/Spec.hs
+++ spec/Spec.hs
@@ -1,5 +1,6 @@
 import Test.Hspec
 import Control.Exception (evaluate)
+import qualified Data.Map.Strict as Map
 
 import Go
 import Go.UI.Color
@@ -22,18 +23,21 @@ main = hspec $ do
                 (addMove newGame point) `shouldBe` (Left "Invalid coordinates")
 
         it "removes a single dead stone" $ do
-            let game0 = newGame
-                Right game1 = addMove game0 $ Point (4, 4)
-                Right game2 = addMove game1 $ Point (4, 3)
-                Right game3 = pass game2
-                Right game4 = addMove game3 $ Point (4, 5)
-                Right game5 = pass game4
-                Right game6 = addMove game5 $ Point (3, 4)
-                Right game7 = pass game6
-                Right game8 = addMove game7 $ Point (5, 4) in
-                boardAt game8 (Point (4, 4)) `shouldBe` Nothing
+            let moves = ["d4", "d3", "pass", "d5", "pass", "c4", "pass", "e4"]
+                game = addMoves moves newGame in
+                boardAt game (Point (4, 4)) `shouldBe` Nothing
 
     describe "Go.pass" $ do
         it "lets the player pass" $ do
             let Right (Game { moves = moves }) = pass newGame in
                 moves `shouldBe` [Nothing]
+
+addMoves :: [String] -> Game -> Game
+addMoves [] game = game
+addMoves ("pass":moves) game = addMoves moves game'
+    where Right game' = pass game
+addMoves ((x:y):moves) game = addMoves moves game'
+    where y' = read y::Int
+          Just x' = Map.lookup x coordLetters
+          point = Point (x', y')
+          Right game' = addMove game point
